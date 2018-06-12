f4f5d74eb0bcc3c565ff28d92c46639031e293aa
Henry Kirse <hkirse@gmail.com>
2018-06-11 20:57:29 -0400

    added serialize and deserialize logic

--- passport.js
+++ passport.js
@@ -3,6 +3,33 @@
 var passport = require('passport')
 , LocalStrategy = require('passport-local').Strategy;
 
+//const User needed for deserializeUser
+const User = require('<relative path to User.js eg ./User>')
+
+//Require SQL to write to db
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+connection.query('USE Jobber');
+
+//export passport function to Jobber app
+module.exports = function(passport){
+
+//Serialize and Deserialize set up
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+    connection.query("SELECT * FROM employer WHERE id = 'email'", function(err,rows){
+        done(err, rows[0]);
+    });
+})
+}
+
 passport.use('logIn', new LocalStrategy(
     function(username, password, done) {
       User.findOne({ username: 'email' }, function(err, user) {
