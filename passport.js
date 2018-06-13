28920622760c3940a2a9f53340b2991ca408cd8a
Henry Kirse <hkirse@gmail.com>
2018-06-12 22:11:19 -0400

    Receiving 404 error...JS passes

--- passport.js
+++ passport.js
@@ -1,58 +1,135 @@
 //Use npm install passport-local
 //Use npm install passport
+//Use npm install mysql
 
 var passport = require('passport')
 , LocalStrategy = require('passport-local').Strategy;
 
-//const User needed for deserializeUser
-//const User = require('<relative path to User.js eg ./User>')
-
-//Require SQL to write to db
 var mysql = require('mysql');
+var express = require("express");
+var bodyParser = require("body-parser");
+var flash = require("flash");
+
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: ''
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password : 'DurhamPortland910',
+    database: "Jobber"
+	});
 
-connection.query('USE Jobber');
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connection started");
+    // run the passport function after the connection is made to prompt the user
+        //passport();
+    });	
 
-//export passport function to Jobber app
-module.exports = function(passport){
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-//Serialize and Deserialize set up
-passport.serializeUser(function(user, done){
-    done(null, user.id);
-});
-passport.deserializeUser(function(id, done){
-    connection.query("SELECT * FROM employer WHERE id = 'email'", function(err,rows){
-        done(err, rows[0]);
+
+// expose this function to our app using module.exports
+//module.exports = function(passport) {
+//function passport() {
+
+	// =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+		done(null, user.id);
     });
-})
-}
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-      User.findOne({  // Using sequelize model function
-          where: { // Take an object with options where self explanatory
-              username: 'email'
-          }
-      }).then(function (user) { // Sequelize return a promise with user in callback
-          if (user == null) { // Checking if user exsists
-              return done(null, false)  // Standard Passport callback
-          }
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+		connection.query("SELECT * FROM employer WHERE id = "+id,function(err,rows){	
+			done(err, rows[0]);
+		});
+    });
+	
 
-          if (password == user.password) { // use your password hash comparing logic here for security
-              return done(null, user) // Standard Passport callback
-          }
-          return done(null, false) // Standard Passport callback
-      })
-  }
-))
+ 	// =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
 
-  //*******Study this**********
-//   app.post('/login', 
-//   passport.authenticate('local', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     res.redirect('/');
-//   });
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) {
+
+		// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+        connection.query("SELECT * FROM employer WHERE email = '"+email+"'",function(err,rows){
+			console.log(rows);
+			console.log("above row object");
+			if (err)
+                return done(err);
+			 if (rows.length) {
+                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+            } else {
+
+				// if there is no user with that email
+                // create the user
+                var newUserMysql = new Object();
+				
+				newUserMysql.email    = email;
+                newUserMysql.password = password; // use the generateHash function in our user model
+			
+				var insertQuery = "INSERT INTO employer ( email, password ) VALUES ('" + email +"','"+ password +"')";
+					console.log(insertQuery);
+				connection.query(insertQuery,function(err,rows){
+				newUserMysql.id = rows.insertId;
+				
+				return done(null, newUserMysql);
+				});	
+            }	
+		});
+    }));
+
+    // =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+
+         connection.query("SELECT * FROM `employer` WHERE `email` = '" + email + "'",function(err,rows){
+			if (err)
+                return done(err);
+			 if (!rows.length) {
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            } 
+			
+			// if the user is found but the password is wrong
+            if (!( rows[0].password == password))
+                return done(null, false, req.flash('loginMessage', 'Incorrect password.')); // create the loginMessage and save it to session as flashdata
+			
+            // all is well, return successful user
+            return done(null, rows[0]);			
+		
+		});
+        
+
+
+    }));
+   	
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
