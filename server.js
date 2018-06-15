ac901e6be25b3213190b1b27e2559d75dd81dcce
Edward Nimocks <enimocks@gmail.com>
2018-06-14 22:41:01 -0400

    change index.js, config.json, & server.js back to test friendly

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require("./routes/api-employee-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 // Start Server
-db.sequelize.sync({force:true}).then(function(){
+db.sequelize.sync({}).then(function(){
 //db.sequelize.sync({}).then(function(){
     app.listen(PORT,function(){
         console.log("Server listening on: http://localhost:" + PORT)
